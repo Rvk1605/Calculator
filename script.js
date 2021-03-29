951e2f13b3f1d33a35b2cfef00b425c289e25280
Rajveer <76820865+Rvk1605@users.noreply.github.com>
2021-03-29 13:09:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,87 +3,108 @@ var display=document.getElementById('display');
 let s="";
 var zero=document.getElementById('zero');
 zero.addEventListener('click',function(){
-    display.innerText='0';
     s=s+'0';
+    display.innerText=s;
+    
 });
 
 var one=document.getElementById('one');
 one.addEventListener('click',function(){
-    display.innerText='1';
     s=s+'1';
+    display.innerText=s;
+   
 });
 
 var two=document.getElementById('two');
 two.addEventListener('click',function(){
-    display.innerText='2';
     s=s+'2';
+    display.innerText=s;
+    
 });
 
 var three=document.getElementById('three');
 three.addEventListener('click',function(){
-    display.innerText='3';
     s=s+'3';
+    display.innerText=s;
+    
 });
 
 var four=document.getElementById('four');
 four.addEventListener('click',function(){
-    display.innerText='4';
     s=s+'4';
+    display.innerText=s;
+    
 });
 
 var five=document.getElementById('five');
 five.addEventListener('click',function(){
-    display.innerText='5';
     s=s+'5';
+    display.innerText=s;
+    
 });
 
 var six=document.getElementById('six');
 six.addEventListener('click',function(){
-    display.innerText='6';
     s=s+'6';
+    display.innerText=s;
+    
 });
 
 var seven=document.getElementById('seven');
 seven.addEventListener('click',function(){
-    display.innerText='7';
     s=s+'7';
+    display.innerText=s;
+    
 });
 
 var eight=document.getElementById('eight');
 eight.addEventListener('click',function(){
-    display.innerText='8';
     s=s+'8';
+    display.innerText=s;
+    
 });
 
 var nine=document.getElementById('nine');
 nine.addEventListener('click',function(){
-    display.innerText='9';
     s=s+'9';
+    display.innerText=s;
+   
 });
 
 var plus=document.getElementById('plus');
 plus.addEventListener('click',function(){
     s=s+'+';
+    display.innerText=s;
 });
 
 var minus=document.getElementById('minus');
 minus.addEventListener('click',function(){
     s=s+'-';
+    display.innerText=s;
 });
 
 var multiply=document.getElementById('multiply');
 multiply.addEventListener('click',function(){
     s=s+'*';
+    display.innerText=s;
 });
 
 var divide=document.getElementById('divide');
 divide.addEventListener('click',function(){
     s=s+'/';
+    display.innerText=s;
 });
 
 var modulo=document.getElementById('modulo');
 modulo.addEventListener('click',function(){
     s=s+'%';
+    display.innerText=s;
+});
+
+var dot=document.getElementById('dot');
+dot.addEventListener('click',function(){
+    s=s+'.';
+    display.innerText=s;
 });
 
 
@@ -99,5 +120,3 @@ equal.addEventListener('click',function(){
     s=String(val);
     display.innerText=val;
 });
-
-// dot operator and sign Not Done
