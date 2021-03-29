7de1ee6b7a7bdcf1ab6d4e490f1a6a8ad5fcaf22
Rajveer <76820865+Rvk1605@users.noreply.github.com>
2021-03-29 13:13:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,70 +5,60 @@ var zero=document.getElementById('zero');
 zero.addEventListener('click',function(){
     s=s+'0';
     display.innerText=s;
-    
 });
 
 var one=document.getElementById('one');
 one.addEventListener('click',function(){
     s=s+'1';
     display.innerText=s;
-   
 });
 
 var two=document.getElementById('two');
 two.addEventListener('click',function(){
     s=s+'2';
     display.innerText=s;
-    
 });
 
 var three=document.getElementById('three');
 three.addEventListener('click',function(){
     s=s+'3';
     display.innerText=s;
-    
 });
 
 var four=document.getElementById('four');
 four.addEventListener('click',function(){
     s=s+'4';
     display.innerText=s;
-    
 });
 
 var five=document.getElementById('five');
 five.addEventListener('click',function(){
     s=s+'5';
     display.innerText=s;
-    
 });
 
 var six=document.getElementById('six');
 six.addEventListener('click',function(){
     s=s+'6';
     display.innerText=s;
-    
 });
 
 var seven=document.getElementById('seven');
 seven.addEventListener('click',function(){
     s=s+'7';
     display.innerText=s;
-    
 });
 
 var eight=document.getElementById('eight');
 eight.addEventListener('click',function(){
     s=s+'8';
     display.innerText=s;
-    
 });
 
 var nine=document.getElementById('nine');
 nine.addEventListener('click',function(){
     s=s+'9';
     display.innerText=s;
-   
 });
 
 var plus=document.getElementById('plus');
